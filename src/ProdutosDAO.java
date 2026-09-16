b45579621898f5cb90aa6889b42a44d5e1089a67
Aloisio Majoli <aloisio-mj@hotmail.com>
2026-09-16 14:56:21 -0300

    Implementa cadastro de produtos no banco

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,7 +7,7 @@
  *
  * @author Adm
  */
-
+import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -23,20 +23,60 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+         conn = new conectaDAO().connectDB();
+
+    String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+    try {
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+
+        prep.executeUpdate();
+
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+    } catch (SQLException erro) {
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + erro.getMessage());
     }
+}
     
     public ArrayList<ProdutosDTO> listarProdutos(){
+        conn = new conectaDAO().connectDB();
+    
+    String sql = "SELECT * FROM produtos";
+    
+    try {
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
         
-        return listagem;
+        listagem.clear();
+        
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+            
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+            
+            listagem.add(produto);
+        }
+        
+    } catch (SQLException erro) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro.getMessage());
     }
     
+    return listagem;
+}
+    }
+        
+     
+    
+    
     
     
         
-}
+
 
