a9827db15ad57fa246f64e245e875329bba6f7ce
Aloisio Majoli <aloisio-mj@hotmail.com>
2026-09-18 10:01:14 -0300

    Implementa venda de produtos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -69,6 +69,24 @@ public class ProdutosDAO {
     }
     
     return listagem;
+}public void venderProduto(ProdutosDTO produto){
+
+    conn = new conectaDAO().connectDB();
+
+    String sql = "UPDATE produtos SET status = ? WHERE id = ?";
+
+    try {
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, "Vendido");
+        prep.setInt(2, produto.getId());
+
+        prep.executeUpdate();
+
+        JOptionPane.showMessageDialog(null, "Produto vendido com sucesso!");
+
+    } catch (SQLException erro) {
+        JOptionPane.showMessageDialog(null, "Erro ao vender produto: " + erro.getMessage());
+    }
 }
     }
         
